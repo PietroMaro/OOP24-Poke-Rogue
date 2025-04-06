48432a304d6928b83a39659ae8062f9c086225bb
Tverdohleb Egor <tverdegor@gmail.com>
2025-04-06 18:47:24 +0200

    changed name to some file to make it more coherent

--- src/main/java/it/unibo/PokeRogue/PokemonInterface.java
+++
@@ -1,9 +0,0 @@
-package it.unibo.PokeRogue.PokemonUtilities;
-
-import java.util.Map;
-
-public interface PokemonInterface {
-
-    void levelUpStats();
-
-}
---
+++ src/main/java/it/unibo/PokeRogue/utilities/RangeImpl.java
@@ -0,0 +1,79 @@
+package it.unibo.PokeRogue.PokemonUtilities;
+
+import java.util.function.BiFunction;
+
+public class RangeImpl<T extends Comparable<T>> implements Range<T>{
+
+    private T currentMin;
+    private T currentMax;
+    private T currentValue;
+    private BiFunction<T,T,T> incrementFunc;
+    private BiFunction<T,T,T> decrementFunc;
+
+
+
+    public Range(T currentMin,T currentMax,T currentValue,BiFunction<T,T,T> incrementFunc,BiFunction<T,T,T> decrementFunc){
+
+        this.currentMin = currentMin;
+        
+        this.currentMax = currentMax;
+        
+        this.currentValue = currentValue;
+        
+        this.incrementFunc = incrementFunc;
+        
+        this.decrementFunc = decrementFunc;
+        
+    }
+
+    @Override
+    public void increment(T x) {
+        T newValue = this.incrementFunc.apply(currentValue, x);
+        if (newValue.compareTo(currentMax) <= 0) {
+            currentValue = newValue;
+        } else {
+            currentValue = currentMax;
+        }
+    }
+
+    @Override
+    public void decrement(T x) {
+        T newValue = this.decrementFunc.apply(currentValue, x);
+        if (newValue.compareTo(currentMin) >= 0) {
+            currentValue = newValue;
+        } else {
+            currentValue = currentMin;
+        }
+    }
+
+    @Override
+    public T getCurrentMin() {
+        return currentMin;
+    }
+
+    @Override
+    public T getCurrentMax() {
+        return currentMax;
+    }
+
+    @Override
+    public T getCurrentValue() {
+        return currentValue;
+    }
+
+    @Override
+    public void setCurrentMin(T x) {
+        currentMin = x;
+    }
+
+    @Override
+    public void setCurrentMax(T x) {
+        currentMax = x;
+    }
+
+    @Override
+    public void setCurrentValue(T x) {
+        currentValue = x;
+    }
+
+}
