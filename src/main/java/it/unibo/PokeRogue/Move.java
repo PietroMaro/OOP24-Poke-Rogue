40c2253bd1e23baa0a31187b903b28ea56422894
Tverdohleb Egor <tverdegor@gmail.com>
2025-04-06 18:33:08 +0200

    implemented pokemon and created interfaces for pokemon and moves TODO: test them

---
+++ src/main/java/it/unibo/PokeRogue/Move.java
@@ -0,0 +1,24 @@
+package it.unibo.PokeRogue.PokemonUtilities;
+
+public class Move {
+    private Boolean physical;
+    private Enum type;
+    private Float critRate;
+    private Range<Integer> ppUse;
+    private Float accurancy;
+    private Float priority;
+    private String effect; 
+    private int baseDamage;
+
+    public Move(Float accurancy, int baseDamage, Float critRate, String effect, Boolean physical, Range<Integer> ppUse, Float priority, Enum type) {
+        this.accurancy = accurancy;
+        this.baseDamage = baseDamage;
+        this.critRate = critRate;
+        this.effect = effect;
+        this.physical = physical;
+        this.ppUse = ppUse;
+        this.priority = priority;
+        this.type = type;
+    }
+
+}
---
+++ src/main/java/it/unibo/PokeRogue/PokemonInterface.java
@@ -0,0 +1,9 @@
+package it.unibo.PokeRogue.PokemonUtilities;
+
+import java.util.Map;
+
+public interface PokemonInterface {
+
+    void levelUpStats();
+
+}
