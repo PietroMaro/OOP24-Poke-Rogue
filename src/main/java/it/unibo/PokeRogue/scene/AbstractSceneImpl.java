d7dfda1b1035d4d5c8a5d22beecdfbdbf7977c7f
Tommaso Miraglia <tommaso.miraglia@studio.unibo.it>
2025-04-08 12:26:13 +0200

    AbstracScene fixed

--- src/main/java/it/unibo/PokeRogue/scene/AbstractSceneImpl.java
+++ src/main/java/it/unibo/PokeRogue/scene/AbstractSceneImpl.java
@@ -1,5 +1,17 @@
 package it.unibo.PokeRogue.scene;
 
-public abstract class AbstractSceneImpl {
+import it.unibo.PokeRogue.GameEngineImpl;
+import it.unibo.PokeRogue.GraphicEngineImpl;
+import it.unibo.PokeRogue.SavingSystemImpl;
 
+public abstract class AbstractSceneImpl implements Scene {
+    GraphicEngineImpl graphicEngineInstance;
+    GameEngineImpl gameEngineInstance;
+    SavingSystemImpl savingSystemInstance;
+
+    public AbstractSceneImpl() {
+        this.graphicEngineInstance = new GraphicEngineImpl();
+        this.gameEngineInstance = new GameEngineImpl();
+        this.savingSystemInstance = new SavingSystemImpl();
+    }
 }
