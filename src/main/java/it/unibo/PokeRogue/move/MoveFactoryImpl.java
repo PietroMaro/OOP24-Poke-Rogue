d8fc22b38515dd140b6691dad5f802bbfa708e39
Tommaso Miraglia <tommaso.miraglia@studio.unibo.it>
2025-05-01 18:48:37 +0200

    fixing move

--- src/main/java/it/unibo/PokeRogue/move/MoveFactoryImpl.java
+++ src/main/java/it/unibo/PokeRogue/move/MoveFactoryImpl.java
@@ -1,9 +1,8 @@
 package it.unibo.PokeRogue.move;
-import it.unibo.PokeRogue.utilities.Range;
+
 import it.unibo.PokeRogue.SingletonImpl;
 import it.unibo.PokeRogue.utilities.JsonReader;
 import it.unibo.PokeRogue.utilities.JsonReaderImpl;
-import it.unibo.PokeRogue.utilities.RangeImpl;
 import it.unibo.PokeRogue.pokemon.Type;
 
 import java.util.Map;
@@ -37,8 +36,7 @@ public class MoveFactoryImpl extends SingletonImpl implements MoveFactory{
 	private void addMoveToBlueprints(final String moveName){
 		JSONObject moveJson;
         moveJson = jsonReader.readJsonObject(Paths.get("src","pokemon_data","moves",moveName +".json").toString());
-		String name = moveName;
-		Range<Integer> pp = new RangeImpl<Integer>(0, moveJson.getInt("pp"), moveJson.getInt("pp")); 
+		int pp = moveJson.getInt("pp");
 		boolean isPhysical = moveJson.getBoolean("isPhysical");
 		JSONObject effect = moveJson.getJSONObject("effect");
 		int accuracy = moveJson.getInt("accuracy");
@@ -47,7 +45,6 @@ public class MoveFactoryImpl extends SingletonImpl implements MoveFactory{
 		Type type = Type.fromString(moveJson.getString("type"));
 		int priority = moveJson.getInt("priority");
 		final Move newMove = new Move(
-				name,
 				pp,
 				isPhysical,
 				effect,
