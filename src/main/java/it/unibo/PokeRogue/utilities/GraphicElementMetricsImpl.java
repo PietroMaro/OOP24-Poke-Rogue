04bd2950da7c768f14efb60f816d64f87a06de93
PietroMaro <pietro.maretti@icloud.com>
2025-05-26 19:28:39 +0200

    Builded utility class

---
+++ src/main/java/it/unibo/PokeRogue/utilities/GraphicElementMetricsImpl.java
@@ -0,0 +1,5 @@
+package it.unibo.PokeRogue.utilities;
+
+public class GraphicElementMetricsImpl implements GraphicElementMetrics{
+    
+}
---
+++ src/main/java/it/unibo/PokeRogue/utilities/scenesElementPositionsUtil.java
@@ -0,0 +1,17 @@
+package it.unibo.PokeRogue.utilities;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public final class scenesElementPositionsUtil {
+
+
+    public static Map<Integer,GraphicElementMetrics> loadGraphicElementMetricsMap(final String sceneName){
+        final Map<Integer,GraphicElementMetrics> allGraphicElementMetrics = new HashMap<>();
+
+
+
+        return allGraphicElementMetrics;
+
+    }
+}
